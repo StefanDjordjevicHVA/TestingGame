4148811edd6c424e446a5045859606c624ec20fa
Stefan Djordjevic <stefan_djordjevic1991@hotmail.com>
2019-12-11 17:00:58 +0100

    BuildScript

---
+++ RunnerTesting/Assets/Editor/JenkinsBuild.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+
+public class JenkinsBuild
+{
+   static void PreformBuild()
+    {
+        string[] __scenes__ = { "Assests/Scenes/SampleScene.unity" };
+        BuildPipeline.BuildPlayer(__scenes__, "D:/Git/TestBuild", BuildTarget.StandaloneWindows, BuildOptions.None);
+    }
+}
